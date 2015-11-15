c9a354f9f3da53719a65f69c504faf8c79d86c59
SergioSegura <dippe21@hotmail.com>
2015-11-15 20:43:50 +0100

    Initial commit

---
+++ app/src/main/java/ejercicio2/probandoversion5/Probandoversion5Fragment.java
@@ -0,0 +1,22 @@
+package ejercicio2.probandoversion5;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class Probandoversion5Fragment extends Fragment {
+
+    public Probandoversion5Fragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_probandoversion5, container, false);
+    }
+}
